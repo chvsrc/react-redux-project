3c957ad2b25d8ca352b4a577a7b9d6584930f9a9
VENKAT <venkat@VENKATs-MacBook-Pro.local>
2021-09-07 00:15:57 -0400

    Added redux with basics

--- src/index.js
+++ src/index.js
@@ -1,9 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import store from './store'
 
-ReactDOM.render(
-  <React.StrictMode>
-    Test with Inspect
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+/* TEST 1 */
+console.log(store);
+
+/* TEST 2 */
+console.log(store.getState());
+
+/* TEST 3 */
+store.dispatch({
+  type: "bugAdded",
+  payload: { description: "Bug1" }
+});
+store.dispatch({
+  type: "bugRemoved",
+  payload: { id: 1 }
+});
+console.log(store.getState());
+
+/* TEST 4 Subscribe */
+store.subscribe(() => {
+  console.log("Store Changed!", store.getState());
+})
+
+store.dispatch({
+  type: "bugAdded",
+  payload: { description: "Bug1" }
+});
+
+store.dispatch({
+  type: "bugRemoved",
+  payload: { id: 1 }
+});
+
+/* TEST 5 UnSubscribe*/
+const unsubscribe = store.subscribe(() => {
+  console.log("Store Changed!", store.getState());
+})
+
+store.dispatch({
+  type: "bugAdded",
+  payload: { description: "Bug1" }
+});
+
+unsubscribe();
+
+store.dispatch({
+  type: "bugRemoved",
+  payload: { id: 1 }
+});
+
+
+
+var div = document.createElement("H1");
+div.innerHTML = "Test with Inspect";
+document.getElementById("root").appendChild(div);
